47700074391cd5363786e4dbd14c450c41d76024
Alvin Svitzer <alsvitzer@gmail.com>
2015-04-15 17:09:17 -0700

    Adding listener & onClick events for fact button
    Adding button and listener to button to change factview on click

--- FunFacts/app/src/main/java/com/alvinsvitzer/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/alvinsvitzer/funfacts/FunFactsActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,8 +17,37 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        final TextView factLabel = (TextView) findViewById(R.id.funFact);
+
+        Button showFactButton = (Button) findViewById(R.id.FFButton);
+        showFactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Update fact label with a new fact.
+                String fact = getRandomFact();
+                factLabel.setText(fact);
+
+            }
+        });
     }
 
+    public String getRandomFact() {
+
+        ArrayList<String> factList = new ArrayList<String>();
+
+        factList.add("Banging your head against a wall burns 150 calories an hour.");
+        factList.add("Bikinis and tampons invented by men.");
+        factList.add("A toaster uses almost half as much energy as a full-sized oven.");
+        factList.add("A baby octopus is about the size of a flea when it is born.");
+        factList.add("Facebook, Skype and Twitter are all banned in China.");
+
+        int factSpot = (int) (Math.random() * factList.size());
+
+        return factList.get(factSpot);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
