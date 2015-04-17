f94934e0eb58b10f4dbb8d505e62b0f8368a5635
Alvin Svitzer <alsvitzer@gmail.com>
2015-04-17 16:02:17 -0700

    Adding ColorPicker
    Adding ColorPicker class to eventually use to set background color of
    RelativeLayout.

---
+++ FunFacts/app/src/main/java/com/alvinsvitzer/funfacts/ColorPicker.java
@@ -0,0 +1,25 @@
+package com.alvinsvitzer.funfacts;
+
+/**
+ * Created by asvitzer on 4/17/2015.
+ */
+public class ColorPicker {
+
+    public String[] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    
+}
