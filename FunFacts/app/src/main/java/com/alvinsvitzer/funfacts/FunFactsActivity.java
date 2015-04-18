eadcbb44d2061d0267e5dfdca3e08c002f46de24
Alvin Svitzer <alsvitzer@gmail.com>
2015-04-18 15:04:54 -0700

    Changing Color Text With OnClick Listener
    Adding in listener ability to change background color and color of text.

--- FunFacts/app/src/main/java/com/alvinsvitzer/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/alvinsvitzer/funfacts/FunFactsActivity.java
@@ -17,6 +17,7 @@ import java.util.Random;
 public class FunFactsActivity extends Activity {
 
     private FactBook factb = new FactBook();
+    private ColorPicker colorWheel = new ColorPicker();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +25,20 @@ public class FunFactsActivity extends Activity {
         setContentView(R.layout.activity_fun_facts);
 
         final RelativeLayout rl = (RelativeLayout) findViewById(R.id.App);
-
+        final Button showFactButton = (Button) findViewById(R.id.FFButton);
         final TextView factLabel = (TextView) findViewById(R.id.funFact);
 
-        //Create a listener to change the displayed fact once clicked
-        Button showFactButton = (Button) findViewById(R.id.FFButton);
+        //Create a listener to change the displayed fact and colors once button is clicked.
         showFactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 //Update fact label with a new fact.
                 factLabel.setText(factb.getRandomFact());
-                rl.setBackgroundColor(Color.RED);
+                int colorNum = colorWheel.getRandomColor();
+
+                rl.setBackgroundColor(colorNum);
+                showFactButton.setTextColor(colorNum);
 
             }
         });
