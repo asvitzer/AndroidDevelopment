d96b27ec65b24f881f418a0bd8f027653f76f66e
Alvin Svitzer <alsvitzer@gmail.com>
2015-04-21 16:08:20 -0700

    Adding Activity Story and passing name via Intent object

--- AdventureTime/app/src/main/java/com/alvinsvitzer/adventuretime/MainActivity.java
+++ AdventureTime/app/src/main/java/com/alvinsvitzer/adventuretime/MainActivity.java
@@ -1,18 +1,19 @@
 package com.alvinsvitzer.adventuretime;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private EditText mNameEditText;
-    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +26,17 @@ public class MainActivity extends Activity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = mNameEditText.getText();
+                String name = mNameEditText.getText().toString();
+                //Toast.makeText(MainActivity.this,name,Toast.LENGTH_LONG);
+                startStory(name);
             }
         });
+
     }
 
-
+    private void startStory(String name){
+        Intent intent = new Intent(this,StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name),name);
+        startActivity(intent);
+    }
 }
