5e119a5dd93b12d4af736c0d6d44b70d8bd909d0
Alvin Svitzer <alsvitzer@gmail.com>
2015-06-25 21:23:14 -0700

    Implementing Parcelable method to serialize object data sent during intents between activities

--- WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/weather/DailyConditions.java
+++ WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/weather/DailyConditions.java
@@ -1,15 +1,33 @@
 package com.alvinsvitzer.weathervane.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by asvitzer on 6/8/2015.
  */
-public class DailyConditions {
+public class DailyConditions implements Parcelable {
 
     private long mTime;
     private String mSummary;
     private double mTemperatureMax;
     private String mIcon;
     private String mTimeZone;
+
+    private DailyConditions(Parcel in){
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperatureMax = in.readDouble();
+        mIcon = in.readString();
+        mTimeZone = in.readString();
+
+    }
+
+    public DailyConditions(){}
 
     public long getTime() {
         return mTime;
@@ -27,8 +45,8 @@ public class DailyConditions {
         mSummary = summary;
     }
 
-    public double getTemperatureMax() {
-        return mTemperatureMax;
+    public int getTemperatureMax() {
+        return (int) Math.round(mTemperatureMax);
     }
 
     public void setTemperatureMax(double temperatureMax) {
@@ -50,4 +68,49 @@ public class DailyConditions {
     public void setTimeZone(String timeZone) {
         mTimeZone = timeZone;
     }
+
+    public int getIconId(){
+
+        return  Forecast.getIconId(mIcon);
+    }
+
+    public String getDayOfTheWeek(){
+
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimeZone));
+        Date dateTime = new Date(mTime * 1000);
+
+        return formatter.format(dateTime);
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperatureMax);
+        dest.writeString(mIcon);
+        dest.writeString(mTimeZone);
+
+    }
+
+
+    public static final Creator<DailyConditions> CREATOR = new Creator<DailyConditions>() {
+        @Override
+        public DailyConditions createFromParcel(Parcel source) {
+            return new DailyConditions(source);
+        }
+
+        @Override
+        public DailyConditions[] newArray(int size) {
+            return new DailyConditions[size];
+        }
+    };
+
 }
