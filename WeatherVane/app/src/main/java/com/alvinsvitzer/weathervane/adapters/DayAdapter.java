c79a7f1ad9d34c51c7144cd1a8eb18342430a2fa
Alvin Svitzer <alsvitzer@gmail.com>
2015-06-25 21:20:33 -0700

    Adding ViewHolder Pattern to cycle through daily forecast List View

--- WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/adapters/DayAdapter.java
+++ WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/adapters/DayAdapter.java
@@ -1,10 +1,14 @@
 package com.alvinsvitzer.weathervane.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.alvinsvitzer.weathervane.R;
 import com.alvinsvitzer.weathervane.weather.DailyConditions;
 
 /**
@@ -37,6 +41,39 @@ public class DayAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        ViewHolder holder;
+        if(convertView == null){
+
+            //Create brand new view
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+            holder.dayNameLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+
+            convertView.setTag(holder);
+        }
+        else{
+
+            holder = (ViewHolder) convertView.getTag();
+
+        }
+
+        DailyConditions dc = mDays[position];
+        holder.iconImageView.setImageResource(dc.getIconId());
+        holder.temperatureLabel.setText(dc.getTemperatureMax() + "");
+        holder.dayNameLabel.setText(dc.getDayOfTheWeek() + "");
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+
+        ImageView iconImageView; //public by default
+        ImageView circleImageView; //public by default
+        TextView temperatureLabel;
+        TextView dayNameLabel;
+
     }
 }
