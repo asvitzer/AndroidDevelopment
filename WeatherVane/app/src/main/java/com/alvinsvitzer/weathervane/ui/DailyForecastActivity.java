ff3f5ac7443d82b073fcaba37682a9f8b82103c7
Alvin Svitzer <alsvitzer@gmail.com>
2015-06-25 21:25:55 -0700

    Grabbing daily forecast from intent to pass into Adapter class

--- WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/ui/DailyForecastActivity.java
+++ WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/ui/DailyForecastActivity.java
@@ -1,23 +1,31 @@
 package com.alvinsvitzer.weathervane.ui;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.os.Parcelable;
 
 import com.alvinsvitzer.weathervane.R;
+import com.alvinsvitzer.weathervane.adapters.DayAdapter;
+import com.alvinsvitzer.weathervane.weather.DailyConditions;
+
+import java.util.Arrays;
 
 public class DailyForecastActivity extends ListActivity {
+
+    private DailyConditions[] mDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
+        mDays = Arrays.copyOf(parcelables,parcelables.length,DailyConditions[].class);
 
-        setListAdapter(adapter);
+        DayAdapter adapter = new DayAdapter(this, mDays);
     }
 
 
