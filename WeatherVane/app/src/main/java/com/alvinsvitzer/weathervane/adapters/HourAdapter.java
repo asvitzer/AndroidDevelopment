317a671179a46deb428f116226de8c54bc20bfb4
Alvin Svitzer <alsvitzer@gmail.com>
2015-07-08 22:25:12 -0700

    Implementing methods to bind data from model object to RecyclerView

--- WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/adapters/HourAdapter.java
+++ WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/adapters/HourAdapter.java
@@ -1,31 +1,46 @@
 package com.alvinsvitzer.weathervane.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alvinsvitzer.weathervane.R;
+import com.alvinsvitzer.weathervane.weather.HourlyConditions;
 
 /**
  * Created by ALVIN on 7/6/15.
  */
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder>{
 
+    private HourlyConditions[] hours;
+
+    public HourAdapter(HourlyConditions[] hours){
+
+        this.hours = hours;
+    }
+
     @Override
     public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.hourly_list_item, parent, false);
+
+        HourViewHolder viewHolder = new HourViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(HourViewHolder holder, int position) {
-
+        holder.bindHourlyConditions(hours[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return hours.length;
     }
 
     public class HourViewHolder extends RecyclerView.ViewHolder{
@@ -42,6 +57,14 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
             mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
             mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
             mIconLabel = (ImageView) itemView.findViewById(R.id.iconImageView);
+        }
+
+        public void bindHourlyConditions(HourlyConditions hc){
+
+            mTimeLabel.setText(hc.getHour());
+            mSummaryLabel.setText(hc.getSummary());
+            mTemperatureLabel.setText(hc.getTemperature());
+            mIconLabel.setImageResource(hc.getIconId());
         }
     }
 }
