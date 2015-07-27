b07edecf1cf6bb2cf726262d06d5b4ae083f1eba
Alvin Svitzer <alsvitzer@gmail.com>
2015-07-27 14:44:02 -0700

    Overriding onClick method to create toast for hourly recycler list item clicks.

--- WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/adapters/HourAdapter.java
+++ WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/adapters/HourAdapter.java
@@ -1,11 +1,13 @@
 package com.alvinsvitzer.weathervane.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alvinsvitzer.weathervane.R;
 import com.alvinsvitzer.weathervane.weather.HourlyConditions;
@@ -16,10 +18,12 @@ import com.alvinsvitzer.weathervane.weather.HourlyConditions;
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder>{
 
     private HourlyConditions[] hours;
+    private Context mContext;
 
-    public HourAdapter(HourlyConditions[] hours){
+    public HourAdapter(Context context, HourlyConditions[] hours){
 
         this.hours = hours;
+        mContext = context;
     }
 
     @Override
@@ -43,7 +47,7 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
         return hours.length;
     }
 
-    public class HourViewHolder extends RecyclerView.ViewHolder{
+    public class HourViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
 
         public TextView mTimeLabel;
         public TextView mSummaryLabel;
@@ -57,14 +61,31 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
             mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
             mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
             mIconLabel = (ImageView) itemView.findViewById(R.id.iconImageView);
+
+            itemView.setOnClickListener(this);
         }
 
         public void bindHourlyConditions(HourlyConditions hc){
 
             mTimeLabel.setText(hc.getHour());
             mSummaryLabel.setText(hc.getSummary());
-            mTemperatureLabel.setText(hc.getTemperature());
+            mTemperatureLabel.setText(hc.getTemperature() + "");
             mIconLabel.setImageResource(hc.getIconId());
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            String time = mTimeLabel.getText().toString();
+            String temperature = mTemperatureLabel.getText().toString();
+            String summary = mSummaryLabel.getText().toString();
+            String message = String.format("At %s, it will be %s and %s.",
+                                            time,
+                                            temperature,
+                                            summary);
+
+            Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
+
         }
     }
 }
--- WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/ui/HourlyForecastActivity.java
+++ WeatherVane/app/src/main/java/com/alvinsvitzer/weathervane/ui/HourlyForecastActivity.java
@@ -4,28 +4,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.alvinsvitzer.weathervane.R;
+import com.alvinsvitzer.weathervane.adapters.HourAdapter;
 import com.alvinsvitzer.weathervane.weather.HourlyConditions;
 
 import java.util.Arrays;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class HourlyForecastActivity extends ActionBarActivity {
 
     private HourlyConditions[] mHours;
 
+    @InjectView(R.id.recyclerView) RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.inject(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
 
         mHours = Arrays.copyOf(parcelables, parcelables.length, HourlyConditions[].class);
 
-        //DayAdapter adapter = new DayAdapter(this, mHours);
-        //setListAdapter(adapter);
+        HourAdapter adapter = new HourAdapter(this, mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        //Created layout manager to improve performance by using this to determine when to reuse or recycle a view
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        //Used to improve performance since hourly forecast array is a fixed sized
+        mRecyclerView.setHasFixedSize(true);
     }
 
 }
