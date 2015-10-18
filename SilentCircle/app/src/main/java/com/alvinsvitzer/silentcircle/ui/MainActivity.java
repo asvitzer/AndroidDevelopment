570583bef12a3dd415791db1b5d202fb3020ccda
Alvin Svitzer <alsvitzer@gmail.com>
2015-10-18 14:45:44 -0700

    Removing unused imports

--- SilentCircle/app/src/main/java/com/alvinsvitzer/silentcircle/ui/MainActivity.java
+++ SilentCircle/app/src/main/java/com/alvinsvitzer/silentcircle/ui/MainActivity.java
@@ -2,19 +2,12 @@ package com.alvinsvitzer.silentcircle.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.alvinsvitzer.silentcircle.R;
 import com.alvinsvitzer.silentcircle.helper.SectionsPagerAdapter;
