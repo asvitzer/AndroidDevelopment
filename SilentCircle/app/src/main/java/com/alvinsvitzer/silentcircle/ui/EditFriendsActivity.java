3c9c430c489675e8fbe15968a4d1f00f3d177f98
Alvin Svitzer <alsvitzer@gmail.com>
2015-10-19 20:25:25 -0700

    Grabbing names from parse to populate Edit Friends.
    Converting EditFriends to ListView Activity.
    
    Grabbing data from Parse and storing in ListView by way of ArrayAdapter.
    
    Adding error dialog features and loading symbol for user while
    connecting to Parse.

--- SilentCircle/app/src/main/java/com/alvinsvitzer/silentcircle/ui/EditFriendsActivity.java
+++ SilentCircle/app/src/main/java/com/alvinsvitzer/silentcircle/ui/EditFriendsActivity.java
@@ -1,25 +1,45 @@
 package com.alvinsvitzer.silentcircle.ui;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 import com.alvinsvitzer.silentcircle.R;
 import com.alvinsvitzer.silentcircle.model.ParseConstants;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-public class EditFriendsActivity extends AppCompatActivity {
+import java.util.List;
+
+public class EditFriendsActivity extends ListActivity {
+
+    public static final String TAG = EditFriendsActivity.class.getSimpleName();
+    protected List<ParseUser> mUserList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Used for showing the progress bar for actions later on. Needs to run before ContentView is set.
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_friends);
         
        // setUpActionBar();
+
+        //Set List View for activity to allow multiple selection. In this case, friends from Parse.
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
     }
 
@@ -31,9 +51,61 @@ public class EditFriendsActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
+
+        //Activate progress indicator for creating user object and passing into Parse.
+        setProgressBarIndeterminateVisibility(true);
+
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
         query.setLimit(50);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+
+                setProgressBarIndeterminateVisibility(true);
+
+                //Success
+                if (e == null) {
+
+                    mUserList = users;
+                    String userNames[] = new String[mUserList.size()];
+
+                    int x = 0;
+                    for (ParseUser user : mUserList) {
+                        userNames[x] = user.getUsername();
+                        x++;
+                    }
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                            EditFriendsActivity.this,
+                            android.R.layout.simple_list_item_checked,
+                            userNames);
+
+                    setListAdapter(adapter);
+
+                } else {
+
+                    //Send error message to log.
+                    Log.e(TAG, e.getMessage());
+
+                    //Show data validation error as dialog box from Parse exception
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
+
+                    //Set the message of the dialog box from a string resource
+                    builder.setMessage(e.getMessage());
+                    //Set the title of the dialog box from a string resources
+                    builder.setTitle(R.string.error_title);
+                    //Leverage android's stock positive button for ok
+                    builder.setPositiveButton(android.R.string.ok, null);
+
+                    //Create the dialog box and show it
+                    AlertDialog userDialog = builder.create();
+                    userDialog.show();
+                }
+
+            }
+        });
     }
 
     @Override
@@ -55,6 +127,9 @@ public class EditFriendsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
     }
+}
 
