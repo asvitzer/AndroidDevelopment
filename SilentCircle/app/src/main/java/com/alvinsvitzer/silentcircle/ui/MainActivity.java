0720aba12fe2db92d609db106ad02c8cf8cdb707
Alvin Svitzer <alsvitzer@gmail.com>
2015-10-29 21:27:07 -0700

    Adding MediaURI for external storage for image received from camera intent

--- SilentCircle/app/src/main/java/com/alvinsvitzer/silentcircle/ui/MainActivity.java
+++ SilentCircle/app/src/main/java/com/alvinsvitzer/silentcircle/ui/MainActivity.java
@@ -3,7 +3,9 @@ package com.alvinsvitzer.silentcircle.ui;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -12,10 +14,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.alvinsvitzer.silentcircle.R;
 import com.alvinsvitzer.silentcircle.helper.SectionsPagerAdapter;
 import com.parse.ParseUser;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,10 +44,15 @@ public class MainActivity extends AppCompatActivity {
     ViewPager mViewPager;
 
     public static final String TAG = MainActivity.class.getSimpleName();
-    public static final int TAKE_PHOTO_REQUEST = 1;
-    public static final int TAKE_VIDEO_REQUEST = 2;
-    public static final int PICK_PHOTO_REQUEST = 3;
-    public static final int PICK_VIDEO_REQUEST = 4;
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
+
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    public static final int MEDIA_TYPE_VIDEO = 5;
+
+    protected Uri mMediaUri;
 
     //Setting up OnClickListener in member variable area for readability when used later
     protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
@@ -48,8 +61,16 @@ public class MainActivity extends AppCompatActivity {
             switch(which){
                 //Take Picture
                 case 0:
-                    Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(takePhoto, TAKE_PHOTO_REQUEST);
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                    if (mMediaUri == null){
+                        Toast.makeText(MainActivity.this, R.string.error_external_storage, Toast.LENGTH_LONG)
+                        .show();
+                    }
+                    else {
+                        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                        startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+                    }
                 //Take Video
                 case 1:
                 //Choose Picture
@@ -59,7 +80,68 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-    };
+        //getOutputMediaFileUri & isExternalStorageAvailable can be removed out of the
+        //anonymous inner class if needed outside of the OnClickListener
+        private Uri getOutputMediaFileUri(int mediaType) {
+
+            // To be safe, you should check that the SDCard is mounted
+            // using Environment.getExternalStorageState() before doing this.
+            if (isExternalStorageAvailable()) {
+                // get the URI
+
+                // 1. Get the external storage directory
+                String appName = MainActivity.this.getString(R.string.app_name);
+                File mediaStorageDir = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                        appName);
+
+                // 2. Create our subdirectory
+                if (! mediaStorageDir.exists()) {
+                    if (! mediaStorageDir.mkdirs()) {
+                        Log.e(TAG, "Failed to create directory.");
+                        return null;
+                    }
+                }
+
+                // 3. Create a file name
+                // 4. Create the file
+                File mediaFile;
+                Date now = new Date();
+                String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+
+                String path = mediaStorageDir.getPath() + File.separator;
+                if (mediaType == MEDIA_TYPE_IMAGE) {
+                    mediaFile = new File(path + "IMG_" + timestamp + ".jpg");
+                }
+                else if (mediaType == MEDIA_TYPE_VIDEO) {
+                    mediaFile = new File(path + "VID_" + timestamp + ".mp4");
+                }
+                else {
+                    return null;
+                }
+
+                Log.d(TAG, "File: " + Uri.fromFile(mediaFile));
+
+                // 5. Return the file's URI
+                return Uri.fromFile(mediaFile);
+            }
+            else {
+                return null;
+            }
+        }
+
+        //helper method used to check storage is mounted
+        private boolean isExternalStorageAvailable() {
+
+            String state = Environment.getExternalStorageState();
+
+            if(state.equals(Environment.MEDIA_MOUNTED)){
+                return true;
+            }
+            else {
+                return false;}
+        }
+        };
 
     protected void onCreate(Bundle savedInstanceState) {
 
