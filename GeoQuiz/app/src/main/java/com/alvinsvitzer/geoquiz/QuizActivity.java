2a4e1e8391d7d1f005642515fd3af9fc3667b79d
Alvin Svitzer <alsvitzer@gmail.com>
2015-12-12 20:54:35 -0800

    Adding Icon resources, Listeners for cycling through questions, and update questions method

--- GeoQuiz/app/src/main/java/com/alvinsvitzer/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/alvinsvitzer/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.alvinsvitzer.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +14,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mNextButton;
     private TextView mQuestionText;
+
+    public static final String TAG = QuizActivity.class.getSimpleName();
 
     //This should be moved to a model object once it gets more complex
     private TrueFalse[] mQuestionBank = {
@@ -33,17 +36,37 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton = (Button) findViewById(R.id.falseButton);
         mTrueButton = (Button) findViewById(R.id.trueButton);
         mNextButton = (Button) findViewById(R.id.nextButton);
-        mNextButton = (Button) findViewById(R.id.nextButton);
         mQuestionText = (TextView) findViewById(R.id.questionText);
 
-        mQuestionText.setText(mQuestionBank[mCurrentIndex].getQuestion());
+        updateQuestions();
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mCurrentIndex < (mQuestionBank.length - 1)) {
+                    mCurrentIndex++;
+
+                }
+
+                else{
+
+                    Log.v(TAG, "End of question bank. Questions reset");
+                    mCurrentIndex = 0;
+
+                }
+
+                updateQuestions();
+
+            }
+        });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(QuizActivity.this, R.string.toast_incorrect,Toast.LENGTH_SHORT)
-                        .show();
+                checkAnswers(false);
+
 
             }
         });
@@ -52,10 +75,31 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(QuizActivity.this, R.string.toast_correct,Toast.LENGTH_SHORT)
-                        .show();
+                checkAnswers(true);
 
             }
         });
+    }
+
+    private void updateQuestions(){
+
+        mQuestionText.setText(mQuestionBank[mCurrentIndex].getQuestion());
+    }
+
+    private void checkAnswers(boolean userPressedTrue){
+
+        boolean answerTruth = mQuestionBank[mCurrentIndex].isTrueQuestion();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerTruth){
+            messageResId = R.string.toast_correct;
+        }
+        else {
+            messageResId = R.string.toast_incorrect;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
     }
 }
