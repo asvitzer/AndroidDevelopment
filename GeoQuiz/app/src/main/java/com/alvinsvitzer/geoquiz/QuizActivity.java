06d3f6a9e7f10faabca830f616c5168395aa74bc
Alvin Svitzer <alsvitzer@gmail.com>
2015-12-15 18:09:32 -0800

    Creating Landscape Layout

--- GeoQuiz/app/src/main/java/com/alvinsvitzer/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/alvinsvitzer/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,10 +13,12 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mFalseButton;
     private Button mTrueButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionText;
 
-    public static final String TAG = QuizActivity.class.getSimpleName();
+    private static final String TAG = QuizActivity.class.getSimpleName();
+    private static final String KEY_INDEX = "Question_Bank_Index";
 
     //This should be moved to a model object once it gets more complex
     private TrueFalse[] mQuestionBank = {
@@ -31,14 +34,39 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mFalseButton = (Button) findViewById(R.id.falseButton);
         mTrueButton = (Button) findViewById(R.id.trueButton);
-        mNextButton = (Button) findViewById(R.id.nextButton);
+        mNextButton = (ImageButton) findViewById(R.id.nextButton);
+        mPrevButton = (ImageButton) findViewById(R.id.prevButton);
         mQuestionText = (TextView) findViewById(R.id.questionText);
 
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
+
         updateQuestions();
+
+        mQuestionText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mCurrentIndex < (mQuestionBank.length - 1)) {
+                    mCurrentIndex++;
+
+                } else {
+
+                    Log.v(TAG, "End of question bank. Questions reset");
+                    mCurrentIndex = 0;
+
+                }
+
+                updateQuestions();
+
+            }
+        });
 
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +81,27 @@ public class QuizActivity extends AppCompatActivity {
 
                     Log.v(TAG, "End of question bank. Questions reset");
                     mCurrentIndex = 0;
+
+                }
+
+                updateQuestions();
+
+            }
+        });
+
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mCurrentIndex <= (mQuestionBank.length - 1) && mCurrentIndex > 0 ) {
+                    mCurrentIndex--;
+
+                }
+
+                else{
+
+                    Log.v(TAG, "End of question bank. Questions reset");
+                    mCurrentIndex = mQuestionBank.length - 1;
 
                 }
 
@@ -79,6 +128,43 @@ public class QuizActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
     }
 
     private void updateQuestions(){
