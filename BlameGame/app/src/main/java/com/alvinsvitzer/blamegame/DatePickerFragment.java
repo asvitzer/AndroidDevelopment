74f97c4127c8987901e498e63532867542022919
Alvin Svitzer <alsvitzer@gmail.com>
2016-01-14 18:55:53 -0800

    Adding NewInstance for DatePickerFragment to pass arguments between fragments

--- BlameGame/app/src/main/java/com/alvinsvitzer/blamegame/DatePickerFragment.java
+++ BlameGame/app/src/main/java/com/alvinsvitzer/blamegame/DatePickerFragment.java
@@ -5,12 +5,27 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Date;
 
 
 /**
  * Created by Alvin on 1/7/16.
  */
 public class DatePickerFragment extends android.support.v4.app.DialogFragment {
+
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
