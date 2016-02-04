fd117774d4480af3be9ff022c50b327c80f9f869
Alvin Svitzer <alsvitzer@gmail.com>
2016-02-04 09:01:03 -0800

    Creating Master Detail pane for larger devices

--- BlameGame/app/src/main/java/com/alvinsvitzer/blamegame/SingleFragmentActivity.java
+++ BlameGame/app/src/main/java/com/alvinsvitzer/blamegame/SingleFragmentActivity.java
@@ -1,8 +1,8 @@
 package com.alvinsvitzer.blamegame;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,10 +12,15 @@ import android.support.v7.app.AppCompatActivity;
  */
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
