5d8008f516dd82c4c7cf2bdfdd9cc9666bc6cc0f
Alvin Svitzer <alsvitzer@gmail.com>
2016-02-05 11:01:20 -0800

    Implementing Callback Method to determine which layout to use based on screen size

--- BlameGame/app/src/main/java/com/alvinsvitzer/blamegame/CrimeListActivity.java
+++ BlameGame/app/src/main/java/com/alvinsvitzer/blamegame/CrimeListActivity.java
@@ -1,11 +1,14 @@
 package com.alvinsvitzer.blamegame;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+
+import com.alvinsvitzer.blamegame.model.Crime;
 
 /**
  * Created by Alvin on 1/1/16.
  */
-public class CrimeListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks {
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
@@ -14,5 +17,20 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime){
+        if (findViewById(R.id.detail_fragment_container) == null){
+            Intent intent = CrimePagerActivity.newIntent(this, crime.getId());
+            startActivity(intent);
+        }
+        else {
+            Fragment newDetail = CrimeFragment.newInstance(crime.getId());
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment_container, newDetail)
+                    .commit();
+        }
     }
 }
