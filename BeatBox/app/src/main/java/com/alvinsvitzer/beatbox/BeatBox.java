616ffdde3c6c5b317c9077c9fea52408f8e6fd11
Alvin Svitzer <alsvitzer@gmail.com>
2016-02-10 10:50:15 -0800

    Hooking up ViewHolder to cycle through list of sound objects to display in Gridview

--- BeatBox/app/src/main/java/com/alvinsvitzer/beatbox/BeatBox.java
+++ BeatBox/app/src/main/java/com/alvinsvitzer/beatbox/BeatBox.java
@@ -1,5 +1,13 @@
 package com.alvinsvitzer.beatbox;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by asvitzer on 02/08/2016.
  */
@@ -7,5 +15,35 @@ public class BeatBox {
 
     private static final String TAG = "BeatBox";
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
+
+    public BeatBox(Context context){
+        mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds(){
+
+        String[] soundNames = new String[0];
+
+        try{
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        }
+        catch(IOException ioe){
+            Log.e(TAG, "Could not list assets", ioe);
+        }
+
+        for (String filename: soundNames){
+            String assetPath = SOUNDS_FOLDER + "/" + filename;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
+    }
 
 }
--- BeatBox/app/src/main/java/com/alvinsvitzer/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/alvinsvitzer/beatbox/BeatBoxFragment.java
@@ -10,13 +10,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.List;
+
 /**
  * Created by asvitzer on 02/08/2016.
  */
 public class BeatBoxFragment extends Fragment {
 
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance(){
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Nullable
@@ -26,7 +37,7 @@ public class BeatBoxFragment extends Fragment {
 
         RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.fragment_beat_box_recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
-        recyclerView.setAdapter(new SoundAdapter());
+        recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
 
         return v;
     }
@@ -35,6 +46,7 @@ public class BeatBoxFragment extends Fragment {
     private class SoundHolder extends RecyclerView.ViewHolder {
 
         private Button mSoundButton;
+        private Sound mSound;
 
         public SoundHolder(LayoutInflater inflater, ViewGroup container) {
             //super(itemView);
@@ -44,9 +56,21 @@ public class BeatBoxFragment extends Fragment {
             mSoundButton = (Button) itemView.findViewById(R.id.list_item_sound_button);
         }
 
+        public void bindSound(Sound sound){
+            mSound = sound;
+            mSoundButton.setText(mSound.getName());
+        }
+
     }
 
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds){
+            mSounds = sounds;
+        }
+
         public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType){
             LayoutInflater inflater = LayoutInflater.from(getActivity());
             return new SoundHolder(inflater, parent);
@@ -54,12 +78,13 @@ public class BeatBoxFragment extends Fragment {
 
         @Override
         public void onBindViewHolder (SoundHolder soundHolder, int position){
-
+            Sound sound = mSounds.get(position);
+            soundHolder.bindSound(sound);
         }
 
         @Override
         public int getItemCount(){
-            return 0;
+            return mSounds.size();
         }
     }
 
