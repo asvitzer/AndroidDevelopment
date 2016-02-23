7220a92d21c01f066ef5ec5b2d19ebab7b170d95
Alvin Svitzer <alsvitzer@gmail.com>
2016-02-22 21:41:35 -0800

    Adding RecyclerView for List of Returned Applications

--- NerdLauncher/app/src/main/java/com/alvinsvitzer/nerdlauncher/NerdLauncherFragment.java
+++ NerdLauncher/app/src/main/java/com/alvinsvitzer/nerdlauncher/NerdLauncherFragment.java
@@ -1,18 +1,62 @@
 package com.alvinsvitzer.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * Created by Alvin on 2/22/16.
  */
 public class NerdLauncherFragment extends Fragment {
 
+    private static final String TAG = NerdLauncherFragment.class.getSimpleName();
+    private RecyclerView mRecyclerView;
+
     public static Fragment newInstance(){
         return new NerdLauncherFragment();
     }
 
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        setUpAdapter();
+        return v;
+    }
+
+    private void setUpAdapter() {
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
+
+
+    }
 }
 
 
